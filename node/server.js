2485842ef73a916b3ecfed6884be4d0edec2168d
kjgrover <kaje14@bellsouth.net>
2018-10-28 16:14:02 -0600

    fixed routes

--- node/server.js
+++ node/server.js
@@ -18,24 +18,6 @@ app.use(express.static('public'))
 app.use(cors());
 app.use(upload())
 
-app.use(function (req, res, next) {
-
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://104.248.69.73:8080/pdfpost');
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
-  next();
-});
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
