ce9c8b8f4131c8eae25a2869c679fbbc32fefc13
kjgrover <kaje14@bellsouth.net>
2018-10-28 16:25:19 -0600

    fixed file structure

--- server.js
+++ server.js
@@ -29,34 +29,6 @@ app.use(bodyParser.json());
 require("./node/routes/apiRoutes")(app);
 require("./node/routes/htmlRoutes")(app);
 
-
-
-app.post("/pdfpost", function(req, res) {
-
-
-  if(req.files){
-    //foo is the post key name for the file
-      var file = req.files.foo,
-      filename = file.name
-  
-      file.mv("./node/pdfs/" +filename).then(res.send("The file was saved!"))
-  }
-
-});
-
-  app.get("/pdfdelete", function(req, res) {
-      console.log(req.params)
-      fs.unlink("./node/pdfs/"+req.query.filename, (err) => {
-          if (err) {
-              res.send("failed to delete local image:"+err);
-          } else {
-              console.log('successfully deleted local image')
-              res.send("The file was deleted!");                                
-          }
-  });
-
-    });
-
 // Starts the server to begin listening
 // =============================================================
 app.listen(PORT, function() {
