98d1ea97f3d577d281425170f950b52406d67cdb
kjgrover <kaje14@bellsouth.net>
2018-10-27 21:53:05 -0600

    working API

--- plumber.R
+++ plumber.R
@@ -1,9 +1,35 @@
 
 
 library(plumber)
+library(tabulizer)
+
 
 #' @get /echo
 function(){
   return("PLUMBER WORKS!")
   
-  }
+}
+
+#' @param pdf
+#' @get /tab
+#' @json
+function(pdf){
+  library("tabulizer")
+  f <- paste("C:/Users/kelse/Desktop/Rscripts/Tessaract/",toString(pdf, width = NULL), sep="")
+  extract_tables(f)
+}
+
+#' @param pdf
+#' @post /post
+#' @json
+function(req){
+  # library("tabulizer")
+  # f <- req$postBody
+  # extract_tables(f)
+  # extract_tables(req$postBody)
+  # content <- read_feather(data)
+  # somefile <- readBin(con = formContents$upload$tempfile, "raw", 
+  #                     n = file.size(formContents$upload$tempfile))
+  # req$postBody
+  # extract_tables(content)
+}
