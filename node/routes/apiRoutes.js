a67eb77471d071acf1967c3228e063a462ede3a7
kjgrover <kaje14@bellsouth.net>
2018-11-05 20:15:16 -0700

    added ocr functionality

--- node/routes/apiRoutes.js
+++ node/routes/apiRoutes.js
@@ -12,7 +12,9 @@ app.post("/pdfpost", function(req, res) {
         var file = req.files.foo,
         filename = file.name
     
-        file.mv("./node/pdfs/" +filename).then(res.send(filename+" saved"))
+        file.mv("./node/png/" +filename).then(res.send(filename+" saved"))
+
+        fs.writeFile("./node/pdf/"+req.query.filename)
 
     }
 
@@ -47,6 +49,106 @@ app.post("/pdfpost", function(req, res) {
         });
 
     });
+
+
+    app.get("/ocr", function(req, res) {
+        console.log(req.params)
+
+        // Demo sample using ABBYY Cloud OCR SDK from Node.js
+
+        if (((typeof process) == 'undefined') || ((typeof window) != 'undefined')) {
+            throw new Error("This code must be run on server side under NodeJS");
+        }
+
+        // Name of application you created
+        var appId = 'rocr_app';
+        // Password should be sent to your e-mail after application was created
+        var password = 'Yt/3zJ20k7nYJSok8G3kR1tw ';
+
+        var imagePath = "./node/png/" + req.query.filename;
+        var outputPath = './node/pdf/' + req.query.filename;
+
+        try {
+            console.log("ABBYY Cloud OCR SDK Sample for Node.js");
+
+            var ocrsdkModule = require('./ocrsdk.js');
+            var ocrsdk = ocrsdkModule.create(appId, password);
+            ocrsdk.serverUrl = "https://cloud.ocrsdk.com"; // change to https for secure connection
+
+            if (appId.length == 0 || password.length == 0) {
+                throw new Error("Please provide your application id and password!");
+            }
+            
+            if( imagePath == 'myFile.jpg') {
+                throw new Error( "Please provide path to your image!")
+            }
+
+            function downloadCompleted(error) {
+                if (error) {
+                    console.log("Error: " + error.message);
+                    return;
+                }
+                console.log("Done.");
+            }
+
+            function processingCompleted(error, taskData) {
+                if (error) {
+                    console.log("Error: " + error.message);
+                    return;
+                }
+
+                if (taskData.status != 'Completed') {
+                    console.log("Error processing the task.");
+                    if (taskData.error) {
+                        console.log("Message: " + taskData.error);
+                    }
+                    return;
+                }
+
+                console.log("Processing completed.");
+                console.log("Downloading result to " + outputPath);
+
+                ocrsdk
+                        .downloadResult(taskData.resultUrl.toString(), outputPath,
+                                downloadCompleted);
+            }
+
+            function uploadCompleted(error, taskData) {
+                if (error) {
+                    console.log("Error: " + error.message);
+                    return;
+                }
+
+                console.log("Upload completed.");
+                console.log("Task id = " + taskData.id + ", status is " + taskData.status);
+                if (!ocrsdk.isTaskActive(taskData)) {
+                    console.log("Unexpected task status " + taskData.status);
+                    return;
+                }
+
+                ocrsdk.waitForCompletion(taskData.id, processingCompleted);
+            }
+
+            var settings = new ocrsdkModule.ProcessingSettings();
+            // Set your own recognition language and output format here
+            settings.language = "English"; // Can be comma-separated list, e.g. "German,French".
+            settings.exportFormat = "pdfSearchable"; // All possible values are listed in 'exportFormat' parameter description 
+                                        // at http://ocrsdk.com/documentation/apireference/processImage/
+
+            console.log("Uploading image..");
+            ocrsdk.processImage(imagePath, settings, uploadCompleted);
+
+        } catch (err) {
+            console.log("Error: " + err.message);
+        }
+
+
+
+
+    });
+
+
+
 
 };
 
