d182b01ecb094080a8f82db88505787d00633fdf
kjgrover <kaje14@bellsouth.net>
2018-11-11 09:32:18 -0700

    removed

--- node/routes/apiRoutes.js
+++ node/routes/apiRoutes.js
@@ -21,135 +21,135 @@ app.post("/pdfpost", function(req, res) {
 
   });
 
-      app.get("/delete", function(req, res) {
+app.get("/delete", function(req, res) {
 
-        var deletefile = req.query.filename.slice(0,-3)
+    var deletefile = req.query.filename.slice(0,-3)
 
-        fs.unlink("./node/csv/"+deletefile+"csv", (err) => {
-            if (err) {
-                res.send("failed to delete local image:"+err);
-            }
-        });
-
-        fs.unlink("./node/pdfs/"+deletefile+"pdf", (err) => {
-            if (err) {
-                res.send("failed to delete local image:"+err);
-            }
-        });
-
-        fs.unlink("./node/png/"+deletefile+"png", (err) => {
-            if (err) {
-                res.send("failed to delete local image:"+err);
-            } else {
-                res.send("All files successfully deleted");                                
-            }
-        });
+    fs.unlink("./node/csv/"+deletefile+"csv", (err) => {
+        if (err) {
+            res.send("failed to delete local image:"+err);
+        }
     });
+
+    fs.unlink("./node/pdfs/"+deletefile+"pdf", (err) => {
+        if (err) {
+            res.send("failed to delete local image:"+err);
+        }
+    });
+
+    fs.unlink("./node/png/"+deletefile+"png", (err) => {
+        if (err) {
+            res.send("failed to delete local image:"+err);
+        } else {
+            res.send("All files successfully deleted");                                
+        }
+    });
+});
 
     
-    app.get("/csvgrab", function(req, res) {
+app.get("/csvgrab", function(req, res) {
 
-        res.download(path.join(__dirname, '../csv/', req.query.filename));
-      });
-
-
-    app.get("/ocr", function(req, res) {
-        console.log(req.params)
-
-        // Demo sample using ABBYY Cloud OCR SDK from Node.js
-
-        if (((typeof process) == 'undefined') || ((typeof window) != 'undefined')) {
-            throw new Error("This code must be run on server side under NodeJS");
-        }
-
-        // Name of application you created
-        var appId = 'rocr_app';
-        // Password should be sent to your e-mail after application was created
-        var password = 'Yt/3zJ20k7nYJSok8G3kR1tw ';
-
-        var ocrfile = req.query.filename.slice(0,-3)
-
-        var imagePath = "./node/png/" + ocrfile + "png";
-        var outputPath = './node/pdfs/' + ocrfile + "pdf";
-
-        try {
-            console.log("ABBYY Cloud OCR SDK Sample for Node.js");
-
-            var ocrsdkModule = require('./ocrsdk.js');
-            var ocrsdk = ocrsdkModule.create(appId, password);
-            ocrsdk.serverUrl = "https://cloud.ocrsdk.com"; // change to https for secure connection
-
-            if (appId.length == 0 || password.length == 0) {
-                throw new Error("Please provide your application id and password!");
-            }
-            
-            if( imagePath == 'myFile.jpg') {
-                throw new Error( "Please provide path to your image!")
-            }
-
-            function downloadCompleted(error) {
-                if (error) {
-                    console.log("Error: " + error.message);
-                    return;
-                }
-                res.send("Done.");
-            }
-
-            function processingCompleted(error, taskData) {
-                if (error) {
-                    console.log("Error: " + error.message);
-                    return;
-                }
-
-                if (taskData.status != 'Completed') {
-                    console.log("Error processing the task.");
-                    if (taskData.error) {
-                        console.log("Message: " + taskData.error);
-                    }
-                    return;
-                }
-
-                console.log("Processing completed.");
-                console.log("Downloading result to " + outputPath);
-
-                ocrsdk
-                        .downloadResult(taskData.resultUrl.toString(), outputPath,
-                                downloadCompleted);
-            }
-
-            function uploadCompleted(error, taskData) {
-                if (error) {
-                    console.log("Error: " + error.message);
-                    return;
-                }
-
-                console.log("Upload completed.");
-                console.log("Task id = " + taskData.id + ", status is " + taskData.status);
-                if (!ocrsdk.isTaskActive(taskData)) {
-                    console.log("Unexpected task status " + taskData.status);
-                    return;
-                }
-
-                ocrsdk.waitForCompletion(taskData.id, processingCompleted);
-            }
-
-            var settings = new ocrsdkModule.ProcessingSettings();
-            // Set your own recognition language and output format here
-            settings.language = "English"; // Can be comma-separated list, e.g. "German,French".
-            settings.exportFormat = "pdfSearchable"; // All possible values are listed in 'exportFormat' parameter description 
-                                        // at http://ocrsdk.com/documentation/apireference/processImage/
-
-            console.log("Uploading image..");
-            ocrsdk.processImage(imagePath, settings, uploadCompleted);
-
-        } catch (err) {
-            res.send("Error: " + err.message);
-        }
-
-
-
-
+    res.download(path.join(__dirname, '../csv/', req.query.filename));
     });
+
+
+app.get("/ocr", function(req, res) {
+    console.log(req.params)
+
+    // Demo sample using ABBYY Cloud OCR SDK from Node.js
+
+    if (((typeof process) == 'undefined') || ((typeof window) != 'undefined')) {
+        throw new Error("This code must be run on server side under NodeJS");
+    }
+
+    // Name of application you created
+    var appId = 'rocr_app';
+    // Password should be sent to your e-mail after application was created
+    var password = 'Yt/3zJ20k7nYJSok8G3kR1tw ';
+
+    var ocrfile = req.query.filename.slice(0,-3)
+
+    var imagePath = "./node/png/" + ocrfile + "png";
+    var outputPath = './node/pdfs/' + ocrfile + "pdf";
+
+    try {
+        console.log("ABBYY Cloud OCR SDK Sample for Node.js");
+
+        var ocrsdkModule = require('./ocrsdk.js');
+        var ocrsdk = ocrsdkModule.create(appId, password);
+        ocrsdk.serverUrl = "https://cloud.ocrsdk.com"; // change to https for secure connection
+
+        if (appId.length == 0 || password.length == 0) {
+            throw new Error("Please provide your application id and password!");
+        }
+        
+        if( imagePath == 'myFile.jpg') {
+            throw new Error( "Please provide path to your image!")
+        }
+
+        function downloadCompleted(error) {
+            if (error) {
+                console.log("Error: " + error.message);
+                return;
+            }
+            res.send("Done.");
+        }
+
+        function processingCompleted(error, taskData) {
+            if (error) {
+                console.log("Error: " + error.message);
+                return;
+            }
+
+            if (taskData.status != 'Completed') {
+                console.log("Error processing the task.");
+                if (taskData.error) {
+                    console.log("Message: " + taskData.error);
+                }
+                return;
+            }
+
+            console.log("Processing completed.");
+            console.log("Downloading result to " + outputPath);
+
+            ocrsdk
+                    .downloadResult(taskData.resultUrl.toString(), outputPath,
+                            downloadCompleted);
+        }
+
+        function uploadCompleted(error, taskData) {
+            if (error) {
+                console.log("Error: " + error.message);
+                return;
+            }
+
+            console.log("Upload completed.");
+            console.log("Task id = " + taskData.id + ", status is " + taskData.status);
+            if (!ocrsdk.isTaskActive(taskData)) {
+                console.log("Unexpected task status " + taskData.status);
+                return;
+            }
+
+            ocrsdk.waitForCompletion(taskData.id, processingCompleted);
+        }
+
+        var settings = new ocrsdkModule.ProcessingSettings();
+        // Set your own recognition language and output format here
+        settings.language = "English"; // Can be comma-separated list, e.g. "German,French".
+        settings.exportFormat = "pdfSearchable"; // All possible values are listed in 'exportFormat' parameter description 
+                                    // at http://ocrsdk.com/documentation/apireference/processImage/
+
+        console.log("Uploading image..");
+        ocrsdk.processImage(imagePath, settings, uploadCompleted);
+
+    } catch (err) {
+        res.send("Error: " + err.message);
+    }
+
+
+
+
+});
 
 
 
