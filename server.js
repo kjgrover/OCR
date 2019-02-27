e752bf9907d622f419b7c85f90cff5f5d9c745cd
kjgrover <kaje14@bellsouth.net>
2019-02-26 19:41:22 -0700

    changed to port 80

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var path = require('path');
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 8080;
+var PORT = 80;
 
 app.use('/', express.static(path.join(__dirname + '/node/public')))
 
