fe266819e96c61e501e38f029fb6fa8b6ef5a53f
kjgrover <kaje14@bellsouth.net>
2019-03-07 10:27:33 -0700

    fixed ssl

--- node/routes/htmlRoutes.js
+++ node/routes/htmlRoutes.js
@@ -7,7 +7,7 @@ app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
-app.get("/.well-known/pki-validation/", function(req, res) {
+app.get("/.well-known/pki-validation/3218FE1AB9AC2DC63DA63A12CB3B15F5.txt", function(req, res) {
   res.sendFile(path.join(__dirname, "../public/3218FE1AB9AC2DC63DA63A12CB3B15F5.txt"));
 });
 };
