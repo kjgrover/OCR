fc6c372b2ebdf3bac5610e1d0659a59426437717
kjgrover <kaje14@bellsouth.net>
2019-03-06 22:46:19 -0700

    regex problems with post

--- plumber.R
+++ plumber.R
@@ -95,10 +95,10 @@ function(pdf, x1, x2, y1, y2){
   w <- pdf_pagesize(f)
   adj <- w[1,5]/1000
 
-  gsub("%2C", ",", x1)
-  gsub("%2C", ",", x2)
-  gsub("%2C", ",", y1)
-  gsub("%2C", ",", y2)
+  x1 <- gsub("%2C", ",", x1)
+  x2 <- gsub("%2C", ",", x2)
+  y1 <- gsub("%2C", ",", y1)
+  y2 <- gsub("%2C", ",", y2)
 
   mx1 <- as.list(strsplit(x1, ",")[[1]])
   print("mx1 below")
